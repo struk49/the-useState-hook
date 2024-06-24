bd75fbbba49f02df189c209065f0598f0420bfd1
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-24 04:03:14 +0000

    learned how to use hooks in react

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// mport HooksCounter from './components/HooksCounter';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevStae';
+import UseStateWithObjects from './components/UseStateWithObject';
+
+
+function App() {
+  return (
+    <div className="App">
+        {/* <StatefulGreetingWithPrevState />
+      <HooksCounter /> */}
+      <UseStateWithObjects />
+    </div>
+  );
+}
+
+export default App;
